263341dedc7bcf2496dc1d3efda7803ee1d0603e
Felipe Bruckmann <felipebruckmann@hotmail.com.br>
2021-11-01 19:54:45 -0300

    Integration tests for user controller

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class UserControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'POST /user should create a user' do
+    post "/user",  params: {user: {
+      name: "test",
+      email: "test@test.com",
+      password: "test",
+      userRole: "testUser"
+    }}
+
+    assert_response :success
+  end
+
+  test 'POST /user without some fileds should return status 400' do
+    post "/user",  params: {user: {
+      email: "test@test.com",
+      password: "test",
+      userRole: "testUser"
+    }}
+
+    assert_response :bad_request
+  end
+  
+  test 'POST /user with an already registered email should return status 400' do
+    post "/user",  params: {user: {
+      name: "test",
+      email: "test@already.com",
+      password: "test",
+      userRole: "testUser"
+    }}
+
+    assert_response :bad_request
+  end
+
+
 end
