9f448e238fda21b79922f1c5cab62db73f4162ce
Felipe Bruckmann <felipebruckmann@hotmail.com.br>
2021-11-06 17:00:37 -0300

    Adding the domain validation for user e-mail

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,6 +7,13 @@ class UserController < ApplicationController
 
   def create
     isUserAlreadyRegistered = User.find_by email: user_params[:email]
+    companyDomain = User.select(:email).where(userRole: 'ADMIN').first
+    companyDomain = companyDomain[:email].split('@')[1]    
+    userDomain = user_params[:email].split('@')[1] 
+
+    if userDomain != companyDomain
+      return render json: {error: "Este e-mail não esta autorizado a se cadastrar na aplicação"}, status: 401
+    end
 
     if isUserAlreadyRegistered.present? 
       return render json: { error: "Este e-mail já possui cadastro no nosso sistema" }, status: 400
@@ -24,6 +31,4 @@ class UserController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :password, :email, :userRole)
   end
-
-
 end
