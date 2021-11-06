bab64367070490f5c1828720d7baa4626e3eecda
Felipe Bruckmann <felipebruckmann@hotmail.com.br>
2021-11-06 17:11:39 -0300

    Adding new test for wrong domain

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
 # Seeding the admin accounts ->
 
 adminUserOne = User.create({email: 'adminOne@example.com', name: 'Admin One', password: 'password', userRole: 'ADMIN'});
-
 adminUserTwo = User.create({email: 'adminTwo@example.com', name: 'Admin Two', password: 'password', userRole: 'ADMIN'});
