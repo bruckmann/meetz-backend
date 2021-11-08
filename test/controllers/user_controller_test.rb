c0762e40485c0c6ba658f64fa283312e85ebc007
Felipe Bruckmann <felipebruckmann@hotmail.com.br>
2021-11-08 19:27:08 -0300

    Fix the linting of user_controller test

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -2,47 +2,46 @@ require "test_helper"
 
 class UserControllerTest < ActionDispatch::IntegrationTest
   test 'POST /user should create a user' do
-    post "/user",  params: {user: {
-      name: "test",
-      email: "test3@test.com",
-      password: "test",
-      userRole: "testUser"
+    post '/user',  params: {user: {
+      name: 'test',
+      email: 'test3@test.com',
+      password: 'test',
+      userRole: 'testUser'
     }}
 
     assert_response :success
   end
 
   test 'POST /user without some fileds should return status 400' do
-    post "/user",  params: {user: {
-      email: "test@test.com",
-      password: "test",
-      userRole: "testUser"
+    post '/user',  params: {user: {
+      email: 'test@test.com',
+      password: 'test',
+      userRole: 'testUser'
     }}
 
     assert_response :bad_request
   end
   
   test 'POST /user with an already registered email should return status 400' do
-    post "/user",  params: {user: {
-      name: "test",
-      email: "test@test.com",
-      password: "test",
-      userRole: "testUser"
+    post '/user',  params: {user: {
+      name: 'test',
+      email: 'test@test.com',
+      password: 'test',
+      userRole: 'testUser'
     }}
 
     assert_response :bad_request
   end
 
   test 'POST /user with a wrong domain should return status 401' do 
-    post "/user",  params: {user: {
-      name: "test",
-      email: "test@differetDomain.com",
-      password: "test",
-      userRole: "testUser"
+    post '/user',  params: {user: {
+      name: 'test',
+      email: 'test@differetDomain.com',
+      password: 'test',
+      userRole: 'testUser'
     }}
 
     assert_response :unauthorized
   end
-
 
 end
