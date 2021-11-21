256b7d22df9c1ca597c9ae45490173c8f6424d4a
Felipe Bruckmann <felipebruckmann@hotmail.com.br>
2021-11-21 12:04:00 -0300

    Testing the authentication flow and login

--- test/controllers/authentication_controller_test.rb
+++ test/controllers/authentication_controller_test.rb
@@ -2,7 +2,25 @@ require "test_helper"
 
 class AuthenticationControllerTest < ActionDispatch::IntegrationTest
   test 'Any route called without the token should return unauthorized' do
-    post '/user/'
+    get '/user/'
     assert_response :unauthorized 
+  end
+
+  test 'Login with the right credentials should return status-code 200' do
+    post '/login', params: {
+      email: 'test@test.com',
+      password: 'secret'
+    }
+
+    assert_response :success
+  end
+
+  test 'Login with the wrong credentials should return status-code 401' do
+    post '/login', params: {
+      email: 'test@test.com',
+      password: 'Wrong Password'
+    } 
+
+    assert_response :unauthorized
   end
 end
