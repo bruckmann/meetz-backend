de3edbfc30a12724a084a3dbbb21abd60aac439a
Felipe Bruckmann <felipebruckmann@hotmail.com.br>
2021-11-22 19:41:06 -0300

    Creating the SQL script

--- test/controllers/authentication_controller_test.rb
+++ test/controllers/authentication_controller_test.rb
@@ -7,7 +7,8 @@ class AuthenticationControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'Login with the right credentials should return status-code 200' do
-    post '/login', params: {
+    post '/login', 
+    params: {
       email: 'test@test.com',
       password: 'secret'
     }
@@ -16,7 +17,8 @@ class AuthenticationControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'Login with the wrong credentials should return status-code 401' do
-    post '/login', params: {
+    post '/login',
+    params: {
       email: 'test@test.com',
       password: 'Wrong Password'
     } 
